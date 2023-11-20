b4456cc985fea50bf8172a6ef0e938fb73bc192a
Marvin Krause <krause.marvin@proton.me>
2023-11-20 06:38:56 +0100

    remove doom themes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -13,12 +13,6 @@
 (require 'ide)
 (require 'presentation)
 
-;; (use-package doom-themes
-;;   :config
-;;   (setq  doom-themes-enable-bold t
-;; 	 doom-themes-enable-italic t)
-;;   (load-theme 'doom-one t)
-;;   (doom-themes-org-config))
 (use-package rustic
   :custom
   (rustic-lsp-client 'eglot))
