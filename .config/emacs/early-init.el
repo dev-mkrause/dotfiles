d126f7c667747fc333f214ba8d26a41fe9cdfb5b
Marvin Krause <krause.marvin@proton.me>
2024-06-20 13:20:21 +0200

    update early-init.el

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,7 +1,16 @@
-(setq gc-cons-threshold 10000000)
+(setq gc-cons-threshold most-positive-fixnum)
 (setq byte-compile-warnings '(not obsolete))
 (setq warning-suppress-log-types '((comp) (bytecomp)))
 (setq native-comp-async-report-warnings-errors 'silent)
+
+
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t)
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
